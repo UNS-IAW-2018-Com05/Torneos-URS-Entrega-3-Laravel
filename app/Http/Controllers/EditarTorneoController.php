794112225af0dd3248c35b0deb4537f4d54cc311
tfelder <tomas.felder@gmail.com>
2018-05-31 12:30:30 -0300

    Se puede seleccionar editor

--- app/Http/Controllers/EditarTorneoController.php
+++ app/Http/Controllers/EditarTorneoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Fechas;
 use App\Partidos;
+use App\Usuarios;
 use App\Helpers\JoinTablas;
 
 class EditarTorneoController extends Controller
@@ -12,7 +13,9 @@ class EditarTorneoController extends Controller
     public function index(){
       $helper = new JoinTablas();
       $fechas = $helper->juntarTablas(Fechas::all());
+      $editores = Usuarios::where('editor',true)->get();
       return view('editarTorneo', [ 'title' => 'Admin Torneos URS',
-                                    'fechas' => $fechas]);
+                                    'fechas' => $fechas,
+                                  'editores' => $editores]);
     }
 }
