6cf9f97c0eca0a58605c7ee2e5b742376dcf804a
igsub <igsub@hotmail.com>
2018-06-08 21:05:04 -0300

    Log out button.

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class IndexController extends Controller
 {
   public function index(){
-    return view('layout', [ 'title' => 'Admin Torneos URS']);
+    return view('layout', [ 'title' => 'Admin Torneos URS' , 'admin'=>Auth::user()->name]);
   }
 }
