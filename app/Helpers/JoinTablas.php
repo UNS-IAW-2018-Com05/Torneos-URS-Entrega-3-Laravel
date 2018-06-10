0bda7f3605369425d0a4cd8ce770faf7b9143f7e
tomasfelder <tomas.felder@gmail.com>
2018-06-10 13:00:05 -0700

    Nuevo torneo aleatorio funcionando

--- app/Helpers/JoinTablas.php
+++ app/Helpers/JoinTablas.php
@@ -8,7 +8,11 @@ class JoinTablas{
 
   public function juntarTablas($fechas){
     foreach ($fechas as &$fecha) {
-      $fecha->partidos = Partidos::findMany($fecha->partidos);
+      $fecha->partidos = Partidos::findMany($fecha->partidos)->where('estado','no iniciado');
+      $dias = $fecha->diasDeJuego;
+      $dias[0] = $dias[0]->toDateTime()->format('d-m-Y');
+      $dias[1] = $dias[1]->toDateTime()->format('d-m-Y');;
+      $fecha->diasDeJuego = $dias;
       foreach ($fecha->partidos as &$partido){
         $partido->local = Clubes::where('_id', new \MongoDB\BSON\ObjectId($partido->local))->first()->nombre;
         $partido->visitante = Clubes::where('_id', new \MongoDB\BSON\ObjectId($partido->visitante))->first()->nombre;
